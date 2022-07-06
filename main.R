2626d3e567a9e0e253f8d4fe821c96aa3607c1c9
rCarto <timothee.giraud@cnrs.fr>
2022-07-06 17:03:18 +0200

    feat: add graticule, header and waterline

--- main.R
+++ main.R
@@ -16,6 +16,31 @@ bt_layer <-function(bertin, geojson, fill,
   res <- c(as.list(environment()), list(...))
   not_empty <- function(x){sum(nchar(x))>0}
   res <- res[unlist(lapply(X = res, FUN = not_empty))]
+  res$bertin <- NULL
+  # manage un set object
+  if(missing(bertin)){
+    bertin <- vector("list")
+  }
+  # manage later position
+  if(!"layers" %in% names(bertin)){
+    lb <- 1
+  }else{
+    lb <- length(bertin$layers) + 1
+  }
+  # use fixed type
+  res$type <- "layer"
+  bertin$layers[[lb]] <- res
+  return(bertin)
+}
+
+
+bt_graticule <-function(bertin, strokeWidth, strokeDasharray,
+                        stroke, step, ...){
+  # get fun args and values
+  res <- c(as.list(environment()), list(...))
+  not_empty <- function(x){sum(nchar(x))>0}
+  res <- res[unlist(lapply(X = res, FUN = not_empty))]
+  res$bertin <- NULL
 
   # manage un set object
   if(missing(bertin)){
@@ -28,10 +53,57 @@ bt_layer <-function(bertin, geojson, fill,
     lb <- length(bertin$layers) + 1
   }
   # use fixed type
-  res$type <- "layer"
+  res$type <- "graticule"
   bertin$layers[[lb]] <- res
   return(bertin)
 }
+
+
+bt_waterlines <-function(bertin, geojson, dist, nb, ...){
+  # get fun args and values
+  res <- c(as.list(environment()), list(...))
+  not_empty <- function(x){sum(nchar(x))>0}
+  res <- res[unlist(lapply(X = res, FUN = not_empty))]
+  res$bertin <- NULL
+  # manage un set object
+  if(missing(bertin)){
+    bertin <- vector("list")
+  }
+  # manage later position
+  if(!"layers" %in% names(bertin)){
+    lb <- 1
+  }else{
+    lb <- length(bertin$layers) + 1
+  }
+  # use fixed type
+  res$type <- "waterlines"
+  bertin$layers[[lb]] <- res
+  return(bertin)
+}
+
+bt_header <-function(bertin, text, fontsize, ...){
+  # get fun args and values
+  res <- c(as.list(environment()), list(...))
+  not_empty <- function(x){sum(nchar(x))>0}
+  res <- res[unlist(lapply(X = res, FUN = not_empty))]
+  res$bertin <- NULL
+  # manage un set object
+  if(missing(bertin)){
+    bertin <- vector("list")
+  }
+  # manage later position
+  if(!"layers" %in% names(bertin)){
+    lb <- 1
+  }else{
+    lb <- length(bertin$layers) + 1
+  }
+  # use fixed type
+  res$type <- "header"
+  bertin$layers[[lb]] <- res
+  return(bertin)
+}
+
+
 
 bt_toJSON <- function(x){
   res <- jsonlite::toJSON(x, auto_unbox = T, pretty = T, sf = "geojson")
@@ -41,5 +113,7 @@ bt_toJSON <- function(x){
 bt_export <- function(x, filename = "test.json"){
   jsonlite::write_json(x, filename)
 }
+
+
 
 
---
+++ test.r
@@ -0,0 +1,49 @@
+
+mtq <- sf::st_transform(mapsf::mf_get_mtq(), 4326)
+
+bt_layer(geojson = mtq) |>
+  bt_toJSON() |>
+  bt_export("test1.json")
+
+bt_param(margin = 10, width = 500, projection = "Winkel3", other_undocumented_arg = "toto") |>
+  bt_layer(geojson = mtq, fill = "red") |>
+  bt_toJSON() |>
+  bt_export("test2.json")
+
+
+bt_param(margin = 10, width = 500, projection = "Winkel3") |>
+  bt_layer(geojson = mtq, fill = "red", other_undocumented_arg = "toto") |>
+  bt_toJSON() |>
+  bt_export("test3.json")
+
+bt_param(margin = 10, width = 500, projection = "Winkel3") |>
+  bt_layer(
+    geojson = mtq,
+    fill = list(
+      type = "choro",
+      values = "CHOM",
+      breaks = c(78.00, 350.75, 839.00, 1659.00, 10046.00),
+      leg_round = -1,
+      leg_title = "Taux de chômage\n. pers",
+      leg_x = 410,
+      leg_y = 10
+    )
+  ) |>
+  bt_toJSON() |>
+  bt_export("test4.json")
+
+
+bt_param(margin = 10, width = 500, projection = "Winkel3", other_undocumented_arg = "toto") |>
+  bt_layer(geojson = mtq, fill = "red") |>
+  bt_graticule(strokeWidth = 1.5, strokeDasharray = 2, stroke  = "green", step  = 0.1)|>
+  bt_toJSON() |>
+  bt_export("test5.json")
+
+bt_param(margin = 10, width = 500, projection = "Winkel3", other_undocumented_arg = "toto") |>
+  bt_waterlines(geojson = mtq, dist = 0.25, nb = 5)|>
+  bt_layer(geojson = mtq, fill = "#808080") |>
+  bt_graticule(strokeWidth = 1.5, strokeDasharray = 2, stroke  = "green", step  = 0.1)|>
+  bt_header(text = "What a Map!\nTrop stylée", fontSize = 40) |>
+  bt_toJSON() |>
+  bt_export("testx.json")
+
