24371873ab47a71a6fbf8a604211b3aefe12b4b2
rCarto <timothee.giraud@cnrs.fr>
2022-07-08 13:26:45 +0200

    feat: add footer and bubble, refactor the whole thing

--- main.R
+++ main.R
@@ -4,7 +4,6 @@ bt_param <- function(margin, width, projection, ...){
   res <- c(as.list(environment()), list(...))
   not_empty <- function(x){sum(nchar(x))>0}
   res <- res[unlist(lapply(X = res, FUN = not_empty))]
-
   res <- list(params=res)
   return(res)
 }
@@ -12,101 +11,56 @@ bt_param <- function(margin, width, projection, ...){
 bt_layer <-function(bertin, geojson, fill,
                     tooltip, strokeWidth,
                     strokeDasharray, stroke,step, ...){
-  # get fun args and values
   res <- c(as.list(environment()), list(...))
-  not_empty <- function(x){sum(nchar(x))>0}
-  res <- res[unlist(lapply(X = res, FUN = not_empty))]
-  res$bertin <- NULL
-  # manage un set object
-  if(missing(bertin)){
-    bertin <- vector("list")
-  }
-  # manage later position
-  if(!"layers" %in% names(bertin)){
-    lb <- 1
-  }else{
-    lb <- length(bertin$layers) + 1
-  }
-  # use fixed type
-  res$type <- "layer"
-  bertin$layers[[lb]] <- res
+  res <- clean_input(res, type = "layer")
+  if(missing(bertin)){bertin <- list()}
+  bertin$layers[[length(bertin$layers) + 1]] <- res
   return(bertin)
 }
 
-
-bt_graticule <-function(bertin, strokeWidth, strokeDasharray,
+bt_graticule <- function(bertin, strokeWidth, strokeDasharray,
                         stroke, step, ...){
-  # get fun args and values
   res <- c(as.list(environment()), list(...))
-  not_empty <- function(x){sum(nchar(x))>0}
-  res <- res[unlist(lapply(X = res, FUN = not_empty))]
-  res$bertin <- NULL
-
-  # manage un set object
-  if(missing(bertin)){
-    bertin <- vector("list")
-  }
-  # manage first later
-  if(!"layers" %in% names(bertin)){
-    lb <- 1
-  }else{
-    lb <- length(bertin$layers) + 1
-  }
-  # use fixed type
-  res$type <- "graticule"
-  bertin$layers[[lb]] <- res
+  res <- clean_input(res, type = "graticule")
+  if(missing(bertin)){bertin <- list()}
+  bertin$layers[[length(bertin$layers) + 1]] <- res
   return(bertin)
 }
-
 
 bt_waterlines <-function(bertin, geojson, dist, nb, ...){
-  # get fun args and values
   res <- c(as.list(environment()), list(...))
-  not_empty <- function(x){sum(nchar(x))>0}
-  res <- res[unlist(lapply(X = res, FUN = not_empty))]
-  res$bertin <- NULL
-  # manage un set object
-  if(missing(bertin)){
-    bertin <- vector("list")
-  }
-  # manage later position
-  if(!"layers" %in% names(bertin)){
-    lb <- 1
-  }else{
-    lb <- length(bertin$layers) + 1
-  }
-  # use fixed type
-  res$type <- "waterlines"
-  bertin$layers[[lb]] <- res
+  res <- clean_input(res, type = "waterlines")
+  if(missing(bertin)){bertin <- list()}
+  bertin$layers[[length(bertin$layers) + 1]] <- res
   return(bertin)
 }
 
 bt_header <-function(bertin, text, fontsize, ...){
-  # get fun args and values
   res <- c(as.list(environment()), list(...))
-  not_empty <- function(x){sum(nchar(x))>0}
-  res <- res[unlist(lapply(X = res, FUN = not_empty))]
-  res$bertin <- NULL
-  # manage un set object
-  if(missing(bertin)){
-    bertin <- vector("list")
-  }
-  # manage later position
-  if(!"layers" %in% names(bertin)){
-    lb <- 1
-  }else{
-    lb <- length(bertin$layers) + 1
-  }
-  # use fixed type
-  res$type <- "header"
-  bertin$layers[[lb]] <- res
+  res <- clean_input(res, type = "header")
+  if(missing(bertin)){bertin <- list()}
+  bertin$layers[[length(bertin$layers) + 1]] <- res
   return(bertin)
 }
 
+bt_footer <- function(bertin, text, fontSize, ...){
+  res <- c(as.list(environment()), list(...))
+  res <- clean_input(res, type = "footer")
+  if(missing(bertin)){bertin <- list()}
+  bertin$layers[[length(bertin$layers) + 1]] <- res
+  return(bertin)
+}
 
+bt_bubble <-function(bertin, geojson, values, k, tooltip, ...){
+  res <- c(as.list(environment()), list(...))
+  res <- clean_input(res, type = "bubble")
+  if(missing(bertin)){bertin <- list()}
+  bertin$layers[[length(bertin$layers) + 1]] <- res
+  return(bertin)
+}
 
 bt_toJSON <- function(x){
-  res <- jsonlite::toJSON(x, auto_unbox = T, pretty = T, sf = "geojson")
+  res <- jsonlite::toJSON(x, auto_unbox = T, pretty = F, sf = "geojson")
   res
 }
 
@@ -114,6 +68,10 @@ bt_export <- function(x, filename = "test.json"){
   jsonlite::write_json(x, filename)
 }
 
-
-
+clean_input <- function(res, type){
+  res <- res[unlist(lapply(X = res, FUN = function(x){!is.name(x)}))]
+  res$bertin <- NULL
+  res$type <- type
+  res
+}
 
--- test.r
+++ test.r
@@ -1,4 +1,3 @@
-
 mtq <- sf::st_transform(mapsf::mf_get_mtq(), 4326)
 
 bt_layer(geojson = mtq) |>
@@ -39,11 +38,34 @@ bt_param(margin = 10, width = 500, projection = "Winkel3", other_undocumented_ar
   bt_toJSON() |>
   bt_export("test5.json")
 
-bt_param(margin = 10, width = 500, projection = "Winkel3", other_undocumented_arg = "toto") |>
+
+
+
+
+bt_param(margin = 10, width = 500, projection = "Winkel3", other_undocumented_arg = "toto")|>
+  bt_bubble(geojson = mtq, values = "POP", k = 60, tooltip = "$LIBGEO") |>
   bt_waterlines(geojson = mtq, dist = 0.25, nb = 5)|>
   bt_layer(geojson = mtq, fill = "#808080") |>
   bt_graticule(strokeWidth = 1.5, strokeDasharray = 2, stroke  = "green", step  = 0.1)|>
   bt_header(text = "What a Map!\nTrop stylée", fontSize = 40) |>
+  bt_footer(text="Credit lines, super longsuper longsuper longsuper longsuper long", fontSize = 8) |>
   bt_toJSON() |>
-  bt_export("testx.json")
+  bt_export("test6.json")
 
+
+
+
+library(sf)
+x <- st_read(dsn = "/home/tim/Documents/prj/fisheye-example/data-raw/COMMUNE.shp")
+x <- x[x$INSEE_REG=="11", ]
+x <-st_transform(x, 4326)
+
+
+bt_param(margin = 10, width = 900, projection = "Mercator") |>
+  bt_bubble( geojson = x, values = "POPULATION", k = 60, tooltip = "$NOM_COM")|>
+  bt_layer( geojson = x, fill = "#0969da")|>
+  bt_graticule( strokeWidth = 1.5, strokeDasharray = 2, stroke  = "green", step  = 0.2)|>
+  bt_header( text = "Population communale en IDF", fontSize = 40)|>
+  bt_footer( text="INSEE, IGN, 2022", fontSize = 8)|>
+  bt_toJSON()|>
+  bt_export( "testx.json")
